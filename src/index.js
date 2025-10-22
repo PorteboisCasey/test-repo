e87d9668d7974cc0532a7bac1304d5ddd3b84282
PostG0D <postg0d@MacBook-Pro-de-PostG0D.local>
2025-10-22 15:07:11 +0200

    feat: test dora system metrics

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// Simple application for DORA metrics demonstration
+const app = {
+  name: 'DORA Metrics Demo',
+  version: '1.0.0',
+
+  start() {
+    console.log(`Starting ${this.name} v${this.version}`);
+    console.log('Application is running...');
+  }
+};
+
+app.start();
+
+module.exports = app;
+// Test DORA feature
